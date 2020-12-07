05be29138e89bcac77a6716de706480b74f0d533
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-12-06 21:41:08 -0800

    progress

--- docs/interfaces/python.rst
+++ docs/interfaces/python.rst
@@ -2,3 +2,11 @@ Python
 ======
 
 Python...
+
+Workspace
+---------
+
+.. module:: azureml.workspace
+
+.. autoclass:: Workspace
+    :members:
---
+++ python/azureml/workspace/workspace.py
@@ -0,0 +1,34 @@
+class Workspace:
+    """Azure Machine Learning Workspace
+
+    This class represents an Azure Machine Learning Workspace.
+
+    Parameters
+    ----------
+    name: str (required)
+        name for reference
+
+    Examples
+    --------
+
+    First, import all necessary modules.
+
+    >>> from azureml import Workspace
+
+    Next, create the ``Workpsace`` object.
+
+    >>> ws = Workspace()
+
+    """
+
+    def __init__(
+        self,
+        name,
+    ):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
